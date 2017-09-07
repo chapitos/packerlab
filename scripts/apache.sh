f42d7cd7a96648a5f309b6a1485d911df7228b5b
pam8be <marko.pascan@bosch-si.com>
2017-09-07 13:24:37 +0200

    Added apache index page. Adjusted Packer template and provisioner scripts.

--- scripts/apache.sh
+++ scripts/apache.sh
@@ -3,4 +3,6 @@
 sudo apt-get install --yes --force-yes php5 libapache2-mod-php5 php5-mysql
 sudo apt-get install --yes apache2
 sudo apt-get update > /dev/null
-
+sudo rm -f /var/www/html/index.html
+sudo cp /tmp/index.html /var/www/html/.
+sudo update-rc.d apache2 defaults
